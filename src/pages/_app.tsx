406cc5e2cd866047a61f8a992e5e128d918e0ca3
avnerjose <avnerj.g.r@gmail.com>
2023-02-08 15:29:57 -0300

    Setting up stitches

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Poppins } from "@next/font/google";
+import { globalStyles } from "@/styles/global";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  globalStyles();
+
+  return (
+    <main className={poppins.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
---
+++ stitches.config.ts
@@ -0,0 +1,39 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  theme: {
+    colors: {
+      white: "#FFFFFF",
+      gray900: "#14141B",
+      gray800: "#1D1D25",
+      gray500: "#535459",
+      green400: "#15C196",
+    },
+  },
+  media: {
+    sm: "(max-width: 640px)",
+    md: "(max-width: 768px)",
+    lg: "(max-width: 1024px)",
+    xl: "(max-width: 1280px)",
+    "2xl": "(max-width: 1536px)",
+  },
+  utils: {
+    paddingY: (value: any) => ({
+      paddingTop: value,
+      paddingBottom: value,
+    }),
+    paddingX: (value: any) => ({
+      paddingLeft: value,
+      paddingRight: value,
+    }),
+  },
+});
